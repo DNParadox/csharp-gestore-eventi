05a1d67a97e5e7dbb05a92b0fd9f9af4f610c9d6
DNParadox <m96fr@DESKTOP-NBI01C4>
2022-11-09 16:38:16 +0100

    Aggiungere i file di progetto.

---
+++ Evento.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Schema;
+
+public  class Evento
+{
+    //Variabili
+    public string Titolo
+    {
+        get
+        {
+            return Titolo;
+        }
+            
+        protected set
+        {
+            
+        }
+    }
+    private int Data
+    {
+        get
+        {
+            return Data;
+        }
+        set
+        {
+
+        }
+    }
+    public int CapienzaMassimaEvento { get; private set; }
+    public int NumeroPostiPrenotati { get; private set; }
+
+  
+
+    // Costruttore 
+    public Evento(string titolo, int data, int capienzaMassimaEvento, int numeroPostiPrenotati)
+    {
+        Titolo = titolo;
+        Data = data;
+        CapienzaMassimaEvento = capienzaMassimaEvento;
+        NumeroPostiPrenotati = numeroPostiPrenotati;
+    }
+
+
+    public void PrenotaPosti()
+    {
+        try
+        {
+            if (CapienzaMassimaEvento != NumeroPostiPrenotati)
+            {
+                Console.WriteLine("Quanti posti vuoi prenotare?");
+
+                Console.WriteLine("Attualmente Posti prenotati:{0} " , NumeroPostiPrenotati);
+
+                int AggiungiPosto = Convert.ToInt32(Console.ReadLine());
+
+                int NumeroPostiAggiornato = AggiungiPosto + NumeroPostiPrenotati;
+
+                Console.WriteLine("Ottimo hai riservato: " + AggiungiPosto);
+                Console.WriteLine("Al momento ci sono :" + NumeroPostiAggiornato);
+
+                NumeroPostiPrenotati = NumeroPostiAggiornato;
+
+            }
+            else
+            {
+                Console.WriteLine("Al momento non ci sono posti disponibili, riprova al prossimo Evento");
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+
+    public void DisdiciPosti()
+    {
+        try
+        {
+            if (CapienzaMassimaEvento != NumeroPostiPrenotati)
+            {
+                Console.WriteLine("Attualmente Posti prenotati:{0} " + NumeroPostiPrenotati);
+
+                Console.WriteLine("Quanti posti vuoi prenotare?");
+
+                int DiminuisciPosto = Convert.ToInt32(Console.ReadLine());
+
+                int NumeroPostiAggiornato = NumeroPostiPrenotati - DiminuisciPosto;
+
+                Console.WriteLine("Ottimo hai riservato: " + DiminuisciPosto);
+                Console.WriteLine("Al momento ci sono : {0}" , NumeroPostiAggiornato);
+
+                NumeroPostiPrenotati = NumeroPostiAggiornato;
+
+            }
+            else
+            {
+                Console.WriteLine("Al momento non ci sono posti disponibili, riprova al prossimo Evento");
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    public override string ToString()
+    {
+        return Data.ToString("dd/MM/yyyy");
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+Evento eve = new Evento("Conferenza", 50, 150, 70);
+Console.WriteLine("-------------------------");
+Console.WriteLine("Benvenuto nel Terminale");
+Console.WriteLine("-------------------------");
+
+
+bool menu = true;
+while(menu)
+{
+    Console.WriteLine("-------------------------");
+    Console.WriteLine("Benvenuto nel Terminale");
+    Console.WriteLine("-------------------------");
+
+    int selected = Convert.ToInt32(Console.ReadLine());
+
+    switch (selected)
+    {
+        case 1:
+            eve.PrenotaPosti();
+
+            break;
+        case 2:
+            eve.DisdiciPosti();
+
+            break;
+    }
+  
+        
+}
+   
+
+
+
