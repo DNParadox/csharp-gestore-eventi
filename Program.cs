5fcdddde63b3c1a8e4433c42e205a34c6d5a35fd
DNParadox <m96fr@DESKTOP-NBI01C4>
2022-11-11 08:12:27 +0100

    Milestone 3 . Inizio Milestone 4

--- Program.cs
+++ Program.cs
@@ -1,21 +1,44 @@
 ﻿//Evento eve = new Evento("Conferenza", Convert.ToDateTime("26/05/1996"), 100, 30);
 using System;
-// Logica Terminale 
-Console.WriteLine("Inserisci titolo dell'Evento");
-string titolo = Console.ReadLine();
-Console.WriteLine("Inserisci la data nel formato dd/MM/yyyy");
-string data = Console.ReadLine();
-Console.WriteLine("Quant'è la capienza massima dell'evento?");
-int capienzaMassimaEvento = Convert.ToInt32(Console.ReadLine());
-int numeroPostiPrenotati = 0;
+//LogicaEventoMilestone1();
 
-Evento eve = new Evento(titolo, Convert.ToDateTime(data), capienzaMassimaEvento, numeroPostiPrenotati);
 ProgrammaEventi programmaEventi = RegistrazioneProgrammazioneEvento();
+Evento eve = LogicaEventoMilestone1();
+
+LogicaEventi(programmaEventi, eve);
+
+
+
+
+
+
+
+Evento LogicaEventoMilestone1()
+{
+    Console.WriteLine("Inserisci titolo dell'Evento");
+    string titolo = Console.ReadLine();
+    Console.WriteLine("Inserisci la data nel formato dd/MM/yyyy");
+    string data = Console.ReadLine();
+    Console.WriteLine("Quant'è la capienza massima dell'evento?");
+    int capienzaMassimaEvento = Convert.ToInt32(Console.ReadLine());
+    int numeroPostiPrenotati = 0;
+
+    Evento eve = new Evento(titolo, Convert.ToDateTime(data), capienzaMassimaEvento, numeroPostiPrenotati);
+
+    return eve;
+
+}
+
+
+
+
+
+
 
 
 bool menu = true;
-    while (menu)
-    {
+while (menu)
+{
     try
     {
         Console.WriteLine();
@@ -39,20 +62,44 @@ bool menu = true;
                 break;
         }
     }
-    catch(Exception e)
+    catch (Exception e)
     {
         Console.WriteLine("Devi inserire un numero!");
     }
 
 
-    }
-     
+}
+
 
 ProgrammaEventi RegistrazioneProgrammazioneEvento()
 {
     Console.WriteLine("Inserisci il nome del programma");
     string titolo = Console.ReadLine();
-
+ 
     ProgrammaEventi programmaEventi = new ProgrammaEventi(titolo);
     return programmaEventi;
 }
+
+
+void LogicaEventi(ProgrammaEventi programmaEventi, Evento eve)
+{
+    Console.WriteLine("Quanti eventi ha questo programma?");
+    int NumeroEventi = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+    // Dobbiamo far girare il programma finché non crea NumeroEventi
+    for (int i = 0; i < NumeroEventi; i++)
+    {
+        LogicaEventoMilestone1();
+
+    }
+    Console.WriteLine("Il numero degli eventi è " + NumeroEventi);
+    // Stampa nome del Programma
+    Console.WriteLine("TUTTI GLI EVENTI sono: " + programmaEventi.StampaTuttiEventi);
+
+    Console.WriteLine("Gli eventi sono: " + programmaEventi.StampaEventiLista);
+}
+
+
